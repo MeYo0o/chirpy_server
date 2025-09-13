24999f0e7380cbc22061893cb8d7f05e02ba1cae
Moaz Ahmed <justmeyo2025@gmail.com>
2025-09-14 01:47:24 +0300

    add: JWT feature.

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,7 +2,10 @@ package auth
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -10,7 +13,7 @@ func HashPassword(password string) (string, error) {
 	passByteSli := []byte(password)
 	hashByteSli, err := bcrypt.GenerateFromPassword(passByteSli, 10)
 	if err != nil {
-		return "", fmt.Errorf("couldn't hash the password.")
+		return "", fmt.Errorf("couldn't hash the password: %v", err)
 	}
 	hashedPassStr := string(hashByteSli)
 
@@ -27,4 +30,45 @@ func ComparePasswordHash(password, hash string) error {
 	}
 
 	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: &jwt.NumericDate{
+			Time: time.Now().UTC(),
+		},
+		ExpiresAt: &jwt.NumericDate{
+			Time: time.Time.Add(time.Now().UTC(), expiresIn),
+		},
+		Subject: userID.String(),
+	})
+
+	return jwtToken.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+
+	jwtToken, err := jwt.ParseWithClaims(tokenString, jwt.MapClaims{}, func(t *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("token is invalid or expired: %v", err)
+	}
+
+	expiresAt, err := jwtToken.Claims.GetExpirationTime()
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("couldn't extract the Expiration Date from this token: %v", err)
+	}
+
+	if time.Now().UTC().Sub(expiresAt.Time) > 0 {
+		return uuid.UUID{}, fmt.Errorf("token is expired: %v", err)
+	}
+
+	userID, err := jwtToken.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("couldn't extract userID from this token: %v", err)
+	}
+
+	return uuid.Parse(userID)
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,11 @@
 package auth
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func TestPasswordHashGeneration(t *testing.T) {
 	passwordsToTest := []string{"123456789", "Moaz", "P@$$W0rD", "Hello World as a password"}
@@ -17,4 +22,39 @@ func TestPasswordHashGeneration(t *testing.T) {
 		}
 	}
 
+}
+
+func TestJWTGenerationAndValidation(t *testing.T) {
+	tokenSecret := "ASDAFFDHDFGJHFJTRY#$%#$%#$^#HFDHD!@#!@ASDASDFADADASDAS"
+	testUserUUID, _ := uuid.Parse("55ff04a8-37b4-4e04-8e14-c59041117dba")
+	expiresIn, _ := time.ParseDuration("5s")
+
+	generatedTokenStr, err := MakeJWT(testUserUUID, tokenSecret, expiresIn)
+	if err != nil {
+		t.Errorf("couldn't make JWT: %v", err)
+	}
+
+	_, err = ValidateJWT(generatedTokenStr, tokenSecret)
+	if err != nil {
+		t.Errorf("couldn't validate the token: %v", err)
+	}
+}
+
+func TestJWTGenerationAndValidationAfterTime(t *testing.T) {
+	tokenSecret := "ASDAFFDHDFGJHFJTRY#$%#$%#$^#HFDHD!@#!@ASDASDFADADASDAS"
+	testUserUUID, _ := uuid.Parse("55ff04a8-37b4-4e04-8e14-c59041117dba")
+	expiresIn, _ := time.ParseDuration("5s")
+
+	generatedTokenStr, err := MakeJWT(testUserUUID, tokenSecret, expiresIn)
+	if err != nil {
+		t.Errorf("couldn't make JWT: %v", err)
+	}
+
+	// Sleep for 10 seconds to ensure the token expires
+	// time.Sleep(time.Second * 10)
+
+	_, err = ValidateJWT(generatedTokenStr, tokenSecret)
+	if err != nil {
+		t.Errorf("couldn't validate the token: %v", err)
+	}
 }
